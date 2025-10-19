ca2be1fba55dd75f71231672aa6971cd26f62e05
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2025-10-19 12:07:49 -0300

    fix: annotate default deps instead of calling it to align with best practices

--- src/fastapi_tdd_docker/main.py
+++ src/fastapi_tdd_docker/main.py
@@ -1,11 +1,15 @@
+from typing import Annotated
+
 from fastapi import Depends, FastAPI
 
 from fastapi_tdd_docker.config import Settings, get_settings
 
 app = FastAPI()
 
+SettingsDep = Annotated[Settings, Depends(get_settings)]
+
 
 @app.get('/ping')
-def pong(settings: Settings = Depends(get_settings)):
+def ping(settings: Settings = SettingsDep):
     print(settings)
     return {'ping': 'pong!', 'env': settings.environment, 'test': settings.testing}
